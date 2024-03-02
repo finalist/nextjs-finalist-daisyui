a45554a71ccedc6989620e64cf4d3601c16b54fa
Martin van Amersfoorth <martin.van.amersfoorth@finalist.nl>
2024-03-02 13:02:45 +0100

    Add daisyUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  theme: {},
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        finalist: {
+          primary: '#ff9100',
+          secondary: '#162020',
+          accent: '#e500fe',
+          neutral: '#faefe7',
+          'base-100': '#ffffff',
+          'base-200': '#dfe5e5',
+          'base-300': '#c1cece',
+        },
       },
-    },
+    ],
   },
-  plugins: [],
-};
-export default config;
+}
+export default config
